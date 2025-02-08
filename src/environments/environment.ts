42682b2215d9e42f250ca04dd91b425f35010f60
Jeans <sistemadesignstyle@gmail.com>
2025-02-08 12:15:09 -0500

    actualizando environmets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,49 +1,67 @@
 export const environment = {
   production: true,
-  urlRaiz: 'https://maquimotora.com',
-  apiUrl: 'https://maquimotora.com/api/test_api_crud',
-  apiUrl2: 'https://maquimotora.com/api/test_api',
-  apiUrl3: 'https://maquimotora.com/api/upload',
-  apiUpdateFile: 'https://maquimotora.com/api/update/upload',
-  apiListarFiles: 'https://maquimotora.com/api/listar/files',
-  apiDeleteFile: 'https://maquimotora.com/api/update/upload',
-  apiUrlTrabajador: 'https://maquimotora.com/api/trabajador',
-  apiUrlRegister: 'https://maquimotora.com/api/register',
-  apiUrlLogin: 'https://maquimotora.com/api/login',
-  apiUrlEmail: 'https://maquimotora.com/api/enviar_correo',
-  apiUrlEmpleador: 'https://maquimotora.com/api/empleador',
-  apiUrlUltimoEmpleador: 'https://maquimotora.com/api/ultimo_empleador',
+  urlRaiz: 'https://apimaquimotora.enfocussoluciones.pe',
+  apiUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/test_api_crud',
+  apiUrl2: 'https://apimaquimotora.enfocussoluciones.pe/api/test_api',
+  apiUrl3: 'https://apimaquimotora.enfocussoluciones.pe/api/upload',
+  apiUpdateFile:
+    'https://apimaquimotora.enfocussoluciones.pe/api/update/upload',
+  apiListarFiles:
+    'https://apimaquimotora.enfocussoluciones.pe/api/listar/files',
+  apiDeleteFile:
+    'https://apimaquimotora.enfocussoluciones.pe/api/update/upload',
+  apiUrlTrabajador:
+    'https://apimaquimotora.enfocussoluciones.pe/api/trabajador',
+  apiUrlRegister: 'https://apimaquimotora.enfocussoluciones.pe/api/register',
+  apiUrlLogin: 'https://apimaquimotora.enfocussoluciones.pe/api/login',
+  apiUrlEmail: 'https://apimaquimotora.enfocussoluciones.pe/api/enviar_correo',
+  apiUrlEmpleador: 'https://apimaquimotora.enfocussoluciones.pe/api/empleador',
+  apiUrlUltimoEmpleador:
+    'https://apimaquimotora.enfocussoluciones.pe/api/ultimo_empleador',
   apiUrlActividadUltimoEmpleador:
-    'https://maquimotora.com/api/actividad_ultimo_empleador',
-  apiUrlMision: 'https://maquimotora.com/api/mision',
-  apiUrlMisionActualizar: 'https://maquimotora.com/api/mision/actualizar',
-  apiUrlMisionUpdate: 'https://maquimotora.com/api/mision/update/upload',
-  apiUrl3Carousel: 'https://maquimotora.com/api/carousel/upload',
-  apiUpdateFileCarousel: 'https://maquimotora.com/api/carousel/update/upload',
-  apiListarFilesCarousel: 'https://maquimotora.com/api/carousel/listar/files',
-  apiDeleteFileCarousel: 'https://maquimotora.com/api/carousel/update/upload',
-  apiListarFilesProducto: 'https://maquimotora.com/api/productos',
-  apiListarCursosProducto: 'https://maquimotora.com/api/productos/cursos',
+    'https://apimaquimotora.enfocussoluciones.pe/api/actividad_ultimo_empleador',
+  apiUrlMision: 'https://apimaquimotora.enfocussoluciones.pe/api/mision',
+  apiUrlMisionActualizar:
+    'https://apimaquimotora.enfocussoluciones.pe/api/mision/actualizar',
+  apiUrlMisionUpdate:
+    'https://apimaquimotora.enfocussoluciones.pe/api/mision/update/upload',
+  apiUrl3Carousel:
+    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/upload',
+  apiUpdateFileCarousel:
+    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/update/upload',
+  apiListarFilesCarousel:
+    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/listar/files',
+  apiDeleteFileCarousel:
+    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/update/upload',
+  apiListarFilesProducto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos',
+  apiListarCursosProducto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos/cursos',
   apiListarEspecialidadesProducto:
-    'https://maquimotora.com/api/productos/especialidades',
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos/especialidades',
   apiListarDestacadosProducto:
-    'https://maquimotora.com/api/productos/destacados',
-  apiUrl3Producto: 'https://maquimotora.com/api/productos/subir',
-  apiUpdateFileProducto: 'https://maquimotora.com/api/productos/update',
-  apiDeleteFileProducto: 'https://maquimotora.com/api/productos',
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos/destacados',
+  apiUrl3Producto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos/subir',
+  apiUpdateFileProducto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos/update',
+  apiDeleteFileProducto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/productos',
   /* 3.- ENVIO-CORREO-V1-P2 */
-  apiUrlEmailContacto: 'https://maquimotora.com/api/contacto',
+  apiUrlEmailContacto:
+    'https://apimaquimotora.enfocussoluciones.pe/api/contacto',
   /* /3.- ENVIO-CORREO-V1-P2 */
-  apiListarFilesTestimonio: 'https://maquimotora.com/api/testimonios',
+  apiListarFilesTestimonio:
+    'https://apimaquimotora.enfocussoluciones.pe/api/testimonios',
 
-  postsUrl: 'https://maquimotora.com/api/posts', // Endpoint para posts
-  categoriesUrl: 'https://maquimotora.com/api/categories', // Endpoint para categorías
-  tagsUrl: 'https://maquimotora.com/api/tags', // Endpoint para tags
-  commentsUrl: 'https://maquimotora.com/api/comments', // Endpoint para comentarios
-  usersUrl: 'https://maquimotora.com/api/users', // Endpoint para usuarios
+  postsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/posts', // Endpoint para posts
+  categoriesUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/categories', // Endpoint para categorías
+  tagsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/tags', // Endpoint para tags
+  commentsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/comments', // Endpoint para comentarios
+  usersUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/users', // Endpoint para usuarios
   /* componente1 */
-  dominio: 'https://maquimotora.com',
-  apiBaseUrl: 'https://maquimotora.com/api', // URL base para la API de Laravel
+  dominio: 'https://apimaquimotora.enfocussoluciones.pe',
+  apiBaseUrl: 'https://apimaquimotora.enfocussoluciones.pe/api', // URL base para la API de Laravel
   endpoints: {
     categoria1: '/categoria1',
     tabla1: '/tabla1',
