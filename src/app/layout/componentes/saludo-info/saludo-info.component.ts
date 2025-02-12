2116398681d246a0f976d676416a5a4ea1cf0dc8
Jeans <sistemadesignstyle@gmail.com>
2025-02-12 15:25:10 -0500

    colocando enlaces

--- src/app/layout/componentes/saludo-info/saludo-info.component.ts
+++ src/app/layout/componentes/saludo-info/saludo-info.component.ts
@@ -21,7 +21,7 @@ export class SaludoInfoComponent {
     {
       titulo: 'FINANCIA TU MOTO AQUÍ',
       icono: 'assets/iconos/info/icono1.png',
-      ruta: '/financiacion',
+      ruta: '/formulario-financiacion',
     },
     {
       titulo: 'GARANTÍA Y POST VENTA',
