476bda24e0a7dfb7aa2b34d479ab8ca5527188ec
Jeans <sistemadesignstyle@gmail.com>
2025-04-01 01:20:29 -0500

    corrigiendo importacion

--- src/app/services/services_motos/comparador-motos.service.ts
+++ src/app/services/services_motos/comparador-motos.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+/* import { environment } from '../../../environments/environment.development'; */
+import { environment } from '../../../environments/environment';
 
 /**
  * Interfaz para los modelos disponibles para comparar
