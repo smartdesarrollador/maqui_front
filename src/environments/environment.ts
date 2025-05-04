ff8cde4edaf3a18c6cc7716b78995f4cdb760f99
Jeans <sistemadesignstyle@gmail.com>
2025-05-03 22:21:07 -0500

    cambiando urls environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,70 +1,60 @@
 export const environment = {
   production: true,
-  urlRaiz: 'https://apimaquimotora.enfocussoluciones.pe',
-  apiUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/test_api_crud',
-  apiUrl2: 'https://apimaquimotora.enfocussoluciones.pe/api/test_api',
-  apiUrl3: 'https://apimaquimotora.enfocussoluciones.pe/api/upload',
-  apiUpdateFile:
-    'https://apimaquimotora.enfocussoluciones.pe/api/update/upload',
-  apiListarFiles:
-    'https://apimaquimotora.enfocussoluciones.pe/api/listar/files',
-  apiDeleteFile:
-    'https://apimaquimotora.enfocussoluciones.pe/api/update/upload',
-  apiUrlTrabajador:
-    'https://apimaquimotora.enfocussoluciones.pe/api/trabajador',
-  apiUrlRegister: 'https://apimaquimotora.enfocussoluciones.pe/api/register',
-  apiUrlLogin: 'https://apimaquimotora.enfocussoluciones.pe/api/login',
-  apiUrlEmail: 'https://apimaquimotora.enfocussoluciones.pe/api/enviar_correo',
-  apiUrlEmpleador: 'https://apimaquimotora.enfocussoluciones.pe/api/empleador',
+  urlRaiz: 'https://apimaqui.iatecdigital.com',
+  apiUrl: 'https://apimaqui.iatecdigital.com/api/test_api_crud',
+  apiUrl2: 'https://apimaqui.iatecdigital.com/api/test_api',
+  apiUrl3: 'https://apimaqui.iatecdigital.com/api/upload',
+  apiUpdateFile: 'https://apimaqui.iatecdigital.com/api/update/upload',
+  apiListarFiles: 'https://apimaqui.iatecdigital.com/api/listar/files',
+  apiDeleteFile: 'https://apimaqui.iatecdigital.com/api/update/upload',
+  apiUrlTrabajador: 'https://apimaqui.iatecdigital.com/api/trabajador',
+  apiUrlRegister: 'https://apimaqui.iatecdigital.com/api/register',
+  apiUrlLogin: 'https://apimaqui.iatecdigital.com/api/login',
+  apiUrlEmail: 'https://apimaqui.iatecdigital.com/api/enviar_correo',
+  apiUrlEmpleador: 'https://apimaqui.iatecdigital.com/api/empleador',
   apiUrlUltimoEmpleador:
-    'https://apimaquimotora.enfocussoluciones.pe/api/ultimo_empleador',
+    'https://apimaqui.iatecdigital.com/api/ultimo_empleador',
   apiUrlActividadUltimoEmpleador:
-    'https://apimaquimotora.enfocussoluciones.pe/api/actividad_ultimo_empleador',
-  apiUrlMision: 'https://apimaquimotora.enfocussoluciones.pe/api/mision',
+    'https://apimaqui.iatecdigital.com/api/actividad_ultimo_empleador',
+  apiUrlMision: 'https://apimaqui.iatecdigital.com/api/mision',
   apiUrlMisionActualizar:
-    'https://apimaquimotora.enfocussoluciones.pe/api/mision/actualizar',
+    'https://apimaqui.iatecdigital.com/api/mision/actualizar',
   apiUrlMisionUpdate:
-    'https://apimaquimotora.enfocussoluciones.pe/api/mision/update/upload',
-  apiUrl3Carousel:
-    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/upload',
+    'https://apimaqui.iatecdigital.com/api/mision/update/upload',
+  apiUrl3Carousel: 'https://apimaqui.iatecdigital.com/api/carousel/upload',
   apiUpdateFileCarousel:
-    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/update/upload',
+    'https://apimaqui.iatecdigital.com/api/carousel/update/upload',
   apiListarFilesCarousel:
-    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/listar/files',
+    'https://apimaqui.iatecdigital.com/api/carousel/listar/files',
   apiDeleteFileCarousel:
-    'https://apimaquimotora.enfocussoluciones.pe/api/carousel/update/upload',
-  apiListarFilesProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos',
+    'https://apimaqui.iatecdigital.com/api/carousel/update/upload',
+  apiListarFilesProducto: 'https://apimaqui.iatecdigital.com/api/productos',
   apiListarCursosProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos/cursos',
+    'https://apimaqui.iatecdigital.com/api/productos/cursos',
   apiListarEspecialidadesProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos/especialidades',
+    'https://apimaqui.iatecdigital.com/api/productos/especialidades',
   apiListarDestacadosProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos/destacados',
-  apiUrl3Producto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos/subir',
+    'https://apimaqui.iatecdigital.com/api/productos/destacados',
+  apiUrl3Producto: 'https://apimaqui.iatecdigital.com/api/productos/subir',
   apiUpdateFileProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos/update',
-  apiDeleteFileProducto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/productos',
+    'https://apimaqui.iatecdigital.com/api/productos/update',
+  apiDeleteFileProducto: 'https://apimaqui.iatecdigital.com/api/productos',
   /* 3.- ENVIO-CORREO-V1-P2 */
-  apiUrlEmailContacto:
-    'https://apimaquimotora.enfocussoluciones.pe/api/contacto',
+  apiUrlEmailContacto: 'https://apimaqui.iatecdigital.com/api/contacto',
   /* /3.- ENVIO-CORREO-V1-P2 */
-  apiListarFilesTestimonio:
-    'https://apimaquimotora.enfocussoluciones.pe/api/testimonios',
+  apiListarFilesTestimonio: 'https://apimaqui.iatecdigital.com/api/testimonios',
 
-  postsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/posts', // Endpoint para posts
-  categoriesUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/categories', // Endpoint para categorías
-  tagsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/tags', // Endpoint para tags
-  commentsUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/comments', // Endpoint para comentarios
-  usersUrl: 'https://apimaquimotora.enfocussoluciones.pe/api/users', // Endpoint para usuarios
+  postsUrl: 'https://apimaqui.iatecdigital.com/api/posts', // Endpoint para posts
+  categoriesUrl: 'https://apimaqui.iatecdigital.com/api/categories', // Endpoint para categorías
+  tagsUrl: 'https://apimaqui.iatecdigital.com/api/tags', // Endpoint para tags
+  commentsUrl: 'https://apimaqui.iatecdigital.com/api/comments', // Endpoint para comentarios
+  usersUrl: 'https://apimaqui.iatecdigital.com/api/users', // Endpoint para usuarios
   /* componente1 */
-  dominio: 'https://apimaquimotora.enfocussoluciones.pe',
-  apiBaseUrl: 'https://apimaquimotora.enfocussoluciones.pe/api', // URL base para la API de Laravel
+  dominio: 'https://apimaqui.iatecdigital.com',
+  apiBaseUrl: 'https://apimaqui.iatecdigital.com/api', // URL base para la API de Laravel
   endpoints: {
     categoria1: '/categoria1',
     tabla1: '/tabla1',
   },
-  dominioCredimaquimotora: 'https://tiendamaquimotora.enfocussoluciones.pe',
+  dominioCredimaquimotora: 'https://tiendamaquimotora.enfocussoluciones.com',
 };
